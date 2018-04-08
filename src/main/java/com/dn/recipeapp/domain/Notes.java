1ff4543ae5cabafa67f3fe4d367c6b4f4da1f9c1
debora <deboraupe@gmail.com>
2018-04-08 15:54:27 -0300

    refactoring entities to use lombik @Data

--- src/main/java/com/dn/recipeapp/domain/Notes.java
+++ src/main/java/com/dn/recipeapp/domain/Notes.java
@@ -1,7 +1,10 @@
 package com.dn.recipeapp.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 public class Notes {
 
@@ -23,27 +26,4 @@ public class Notes {
     public Notes() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
 }
